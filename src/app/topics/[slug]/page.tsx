5fdbf5f1c57409277561a01ab29d869c09652ea3
Jonathan Jacka <jonathanjacka@gmail.com>
2024-02-07 16:34:28 -0600

    Adds topic page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const TopicShowPage = () => {
+interface TopicShowPageProps {
+  params: {
+    slug: string
+  }
+}
+
+const TopicShowPage = ({ params}: TopicShowPageProps) => {
+
+  const { slug } = params;
   return (
-    <div>
-      Topic Show Page
+    <div className='grid gird-cols-4 gap-4 p-4'>
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold-mb-2">
+          {slug}
+        </h1>
+      </div>
+
+      <div>Preview Post Button</div>
     </div>
   )
 }
