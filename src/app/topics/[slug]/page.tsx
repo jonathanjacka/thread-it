0f2943454006da80f1f772f0417b28f05be77d67
Jonathan Jacka <jonathanjacka@gmail.com>
2024-02-08 14:21:29 -0600

    Adds postlist and query functions

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from '@/components/posts/PostCreateForm';
+import PostList from '@/components/posts/PostList';
+import { fetchPostsByTopicSlug,  fetchTopicNameBySlug} from '@/db/queries/posts';
 
 interface TopicShowPageProps {
   params: {
@@ -6,15 +8,18 @@ interface TopicShowPageProps {
   }
 }
 
-const TopicShowPage = ({ params }: TopicShowPageProps) => {
+const TopicShowPage = async ({ params }: TopicShowPageProps) => {
 
   const { slug } = params;
+  const topicName = await fetchTopicNameBySlug(slug);
+  
   return (
     <div className='grid grid-cols-4 gap-4 p-4'>
       <div className="col-span-3">
-        <h1 className="text-2xl font-bold-mb-2">
-          {slug}
+        <h1 className="text-2xl font-bold mb-2">
+          {topicName ? topicName : slug}
         </h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
       </div>
 
       <div>
